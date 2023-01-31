1bcb148f07d42e06409f933598bdaef67cd3ce70
JakubPloch <kubaploch34@gmail.com>
2023-01-31 22:18:19 +0100

    Repositories hotfix

--- TMS/src/main/java/com/tms/TMS/Repositories/IRouteRepository.java
+++ TMS/src/main/java/com/tms/TMS/Repositories/IRouteRepository.java
@@ -2,6 +2,8 @@ package com.tms.TMS.Repositories;
 
 import com.tms.TMS.Models.Route;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IRouteRepository extends CrudRepository<Route, Long> {
 }
--- TMS/src/main/java/com/tms/TMS/Repositories/IScheduleRepository.java
+++ TMS/src/main/java/com/tms/TMS/Repositories/IScheduleRepository.java
@@ -2,6 +2,8 @@ package com.tms.TMS.Repositories;
 
 import com.tms.TMS.Models.Schedule;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IScheduleRepository extends CrudRepository<Schedule, Long> {
 }
