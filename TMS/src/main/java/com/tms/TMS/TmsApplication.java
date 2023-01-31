f0a4a13f6ffa68ddbdebca032afeebdbe349eac6
SWojcik800 <43492034+SWojcik800@users.noreply.github.com>
2023-01-31 18:20:11 +0100

    Feature/ag grid (#5)
    * Add grid
    
    * Add angular template
    
    * Add routing

--- TMS/src/main/java/com/tms/TMS/TmsApplication.java
+++ TMS/src/main/java/com/tms/TMS/TmsApplication.java
@@ -3,8 +3,10 @@ package com.tms.TMS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//TODO Configure authentication and CORS
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TmsApplication {
 
 	public static void main(String[] args) {
---
+++ frontend/tmp-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core';
+import { AgGridModule } from 'ag-grid-angular';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    AppRoutingModule,
+    AgGridModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
