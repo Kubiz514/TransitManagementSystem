50bbc4030d908ad4cef4d2e3e04efd5d3b0e25f6
DESKTOP-HGRP1DQ\leon <szymonwojcik800@gmail.com>
2023-02-01 20:18:26 +0100

    Modify app class

--- TMS/src/main/java/com/tms/TMS/TmsApplication.java
+++ TMS/src/main/java/com/tms/TMS/TmsApplication.java
@@ -1,16 +1,46 @@
 package com.tms.TMS;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //TODO Configure authentication and CORS
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class TmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TmsApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedHeaders()
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+			}
+		};
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests((requests) -> requests
+						.requestMatchers("**").permitAll()
+						.anyRequest().anonymous()
+				)
+				.logout((logout) -> logout.permitAll());
+
+		return http.build();
 	}
 
 }
