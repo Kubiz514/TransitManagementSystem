539d71f2d23cc4b93ecb3c42acd38810c5148f15
SWojcik800 <43492034+SWojcik800@users.noreply.github.com>
2023-02-02 20:22:40 +0100

    Feature/generic error dialog (#17)
    * Add grid
    
    * Modify app class
    
    * Add generic error dialog
    
    * fix

--- frontend/tmp-app/src/app/core/web-api/web-api.service.ts
+++ frontend/tmp-app/src/app/core/web-api/web-api.service.ts
@@ -1,18 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environment/environment';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/error-dialog/error-dialog.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebApiService {
 
-  constructor(private _httpClient: HttpClient) { }
+  constructor(private _httpClient: HttpClient, private _dialog: MatDialog) { }
 
-  public get = (url: string, params?: any): Observable<any> => this._httpClient.get(environment.webApiUrl+url, {params});
-  public post = (url: string, params?: any): Observable<any> => this._httpClient.post(environment.webApiUrl+url, params);
-  public put = (url: string, params: any): Observable<any> => this._httpClient.put(environment.webApiUrl+url, params);
-  public patch = (url: string, params: any): Observable<any> => this._httpClient.patch(environment.webApiUrl+url, params);
-  public delete = (url: string, params: any): Observable<any> => this._httpClient.delete(environment.webApiUrl+url, params);
+  public get = (url: string, params?: any): Observable<any> => this._httpClient.get(environment.webApiUrl+url, {params})
+  .pipe(
+    catchError(this._handleErrorFunc)
+  );
+  public post = (url: string, params?: any): Observable<any> => this._httpClient.post(environment.webApiUrl+url, params)
+  .pipe(
+    catchError(this._handleErrorFunc)
+  );
+  public put = (url: string, params: any): Observable<any> => this._httpClient.put(environment.webApiUrl+url, params)
+  .pipe(
+    catchError(this._handleErrorFunc)
+  );
+  public patch = (url: string, params: any): Observable<any> => this._httpClient.patch(environment.webApiUrl+url, params)
+  .pipe(
+    catchError(this._handleErrorFunc)
+  );
+  public delete = (url: string, params: any): Observable<any> => this._httpClient.delete(environment.webApiUrl+url, params)
+  .pipe(
+    catchError(this._handleErrorFunc)
+  );
+
+  private _handleErrorFunc = (err: any) => {
+    this._dialog.open(ErrorDialogComponent, { data: err })
+    console.log(err);
+    return err;
+  }
 }
